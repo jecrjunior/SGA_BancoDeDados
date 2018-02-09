b91bd7a753b6f47f036a2809d8434e44f24d02e2
J. Eurique Cardoso Ribeiro Junior <jecrjunior@dcomp.ufs.br>
2018-02-09 17:01:30 -0300

    SQL DDL Script Final

--- scripts_dml.sql
+++ scripts_dml.sql
@@ -1,15 +1,28 @@
-SELECT 
-		G.id, 
-		G.title, 
-		'guia' as tipo,
-		G.idpai as pai
-	FROM 
-		public.tb_guia G
-UNION
-SELECT 
-		SG.id, 
-		SG.title, 
-		'item' as tipo,
-		SG.idguia as pai
-	FROM 
-		public.tb_item_guia SG
+CREATE FUNCTION get_sons_of(father int)
+RETURNS TABLE
+(id int, title varchar, tipo char, idpai int) AS $
+BEGIN
+	RETURN QUERY
+	SELECT 
+		*
+	FROM ( 
+			SELECT
+				G.id,
+				G.title,
+				'guia' as tipo,
+				G.idpai as pai
+			FROM
+				public.tb_guia G
+		UNION
+			SELECT
+				SG.id,
+				SG.title,
+				'item' as tipo,
+				SG.idguia as pai
+			FROM
+				public.tb_item_guia SG
+	) A
+	WHERE A.pai = father;
+END
+
+$ LANGUAGE plpgsql;
